67eb8450922789d7482775ba1bb4c0651d206166
JohnBrunson <JohnFBrunson@gmail.com>
2024-11-04 17:39:09 -0800

    form test now passes. Still some work in progress

--- assets/js/form.js
+++ assets/js/form.js
@@ -4,14 +4,14 @@ const form = document.querySelector('form');
 // TODO: Create a function that handles the form submission. Grab the form data and store it in local storage, then redirect to the blog page using the `redirectPage` function. If the form is submitted with missing data, display an error message to the user.
 function saveBlog(event) {
     event.preventDefault();
-    console.log("saveBlog was called.")
+    console.log("INFO: saveBlog function was called.")
     const username = document.querySelector('#username');
     const title = document.querySelector('#title');
     const content = document.querySelector('#content')
     if (username.value === "" || title.value === "" || content.value === "") {
        const error = document.querySelector('#error');
        error.textContent = "Please complete the form."
-       console.log("error has occurred.")
+       console.log("ERROR: error has occurred This is likely because there is a missing value for the blog form.")
     }else {
         let blog = {
             username: username.value,
@@ -22,7 +22,7 @@ function saveBlog(event) {
         console.log(`blog: ${blog.title}`);
         console.log(`blog: ${blog.content}`);
     }
-
+    return blog;
 
 }
 
--- assets/js/logic.js
+++ assets/js/logic.js
@@ -7,9 +7,31 @@ let isTest = false
 
 
 // TODO: Create a function called `readLocalStorage` that reads from local storage and returns the data. If no data exists, return an empty array. It does not need to take any arguments, It just needs to return the blogs stored in local storage as an array. Please use the key 'blogs' for storing and retrieving your blogs to/from local storage
+// BRUNSON TODO: This needs Testing... doesn't work yet.
+function readLocalStorage () {
+  console.log ("INFO: readLocalStorage function was called.")
+  console.log(`INFO: localStorage length is currently ${localStorage.length}`);
+  
+  let blogs = [];
+  if (localStorage.length === 0) {
+    return blogs;
+  }
+  else{
 
+  }
+}
 
 // TODO: Create a function called `storeLocalStorage` that takes a given object and saves the new data to the existing blog data in local storage. Please use the key 'blogs' for storing blog data in local storage, otherwise the local tests will not work properly. This function should utilize your readLocalStorage function for getting the existing data
+
+// BRUNSON TODO: Needs testing... seems to be getting called on start by one of the testing scripts?
+function storeLocalStorage() {
+  console.log ("INFO: storeLocalStorage function was called");
+  console.log (`INFO: blog was received as ${blog}`);
+  console.log (`INFO: localStorage length prior to setting is ${localStorage.length}`);
+  localStorage.set('blogs', JSON.stringify(blog));
+  console.log (`INFO: localStorage length after setting is is ${localStorage.length}`);
+}
+
 
 // !!!!! Use the following redirectPage function whenever you need to redirect to a different page.  Do not modify any of the code below
 let redirectURL = '';
